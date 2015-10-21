cb7a538cfaa854caa6577e571b8205a9d48be571
a2vijaya <aruran.vijayaratnam@ryerson.ca>
2015-10-21 16:29:40 -0400

    Added new line

---
+++ Lab_05/src/Lab_05.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : Lab_05.cpp
+// Author      : AruranV
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello World" << endl; // prints Hello World
+	cout << "Goodbye Moon" << endl;
+	return 0;
+}
